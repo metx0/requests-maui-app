92f64a2056a2e0dbd736048a66efb4f7f437a0f9
Miguel Alejandro <migtorruco@gmail.com>
2024-11-12 12:04:55 -0600

    feat: data model added

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,40 @@
-﻿namespace RequestsApp
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text.Json;
+
+namespace RequestsApp
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage, INotifyPropertyChanged
     {
-        int count = 0;
+        private readonly HttpClient _httpClient = new HttpClient();
+        private ObservableCollection<PersonaModel> _people;
+
+        // Property for the field above
+        private ObservableCollection<PersonaModel> People
+        {
+            get => _people;
+            set
+            {
+                _people = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public async void GetData()
+        {
+            var response = await _httpClient
+                .GetStringAsync("https://fi.jcaguilar.dev/v1/escuela/persona");
+            // Populate the collection with PersonaModel objects
+            var people = JsonSerializer
+                .Deserialize<ObservableCollection<PersonaModel>>(response);
+
+            if (people != null)
+                People = people;
+        }
 
         public MainPage()
         {
             InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
         }
     }
 
---
+++ PersonaModel.cs
@@ -0,0 +1,13 @@
+﻿namespace RequestsApp
+{
+    public class PersonaModel
+    {
+        // All the properties defined in the endpoint used to add a person
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Sexo { get; set; }
+        public string Fh_nac { get; set; }
+        public string Id_rol { get; set; }
+    }
+}
