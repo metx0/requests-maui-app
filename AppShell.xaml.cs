55b55bfd51665bd17978f3ccc5ae71141f18090e
Miguel Alejandro <migtorruco@gmail.com>
2024-11-12 11:40:40 -0600

    feat: basic setup

---
+++ AppShell.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace RequestsApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+
+            // Register navigation routes
+            Routing.RegisterRoute(nameof(FormPage), typeof(FormPage));
+        }
+    }
+}
