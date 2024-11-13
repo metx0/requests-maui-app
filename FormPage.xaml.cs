f345aeb350a58b6b5ca40e713aaa54c884e415cb
Miguel Alejandro <migtorruco@gmail.com>
2024-11-13 11:57:09 -0600

    feat: form working now; gotta refactor

--- FormPage.xaml.cs
+++ FormPage.xaml.cs
@@ -31,29 +31,47 @@ public partial class FormPage : ContentPage
         return roles[rol];
     }
 
+    private string mapSex(string sexo)
+    { 
+        var sexos = new Dictionary<string, string>
+        {
+            {"Hombre", "h"},
+            {"Mujer", "m"},
+            {"Otro", "o"},
+        };
+
+        return sexos[sexo];
+    }
+
     private async void btnSubmit_Clicked(object sender, EventArgs e)
     {
         // Validate that all fields are filled
         string nombre = txtNombre.Text;
         string apellido = txtApellido.Text;
-        string sexo = pickSexo.SelectedItem.ToString();
+        string sexo = mapSex(pickSexo.SelectedItem.ToString());
 
         DateTime selectedDate = pickFhNac.Date;
-        string fh_nac = selectedDate.ToString();
+        string fh_nac = selectedDate.ToString("yyyy-MM-dd");
+        Debug.WriteLine("fh_nac: " + fh_nac);
 
         string rol = pickRol.SelectedItem.ToString();
         int id_rol = mapRol(rol);
+        Debug.WriteLine("Id_rol: " + id_rol);
 
         // Create the PersonaModel object
         PersonaModel persona = new PersonaModel(nombre, apellido, sexo, fh_nac, id_rol);
 
         bool result = await sendData(persona);
+        Debug.WriteLine(result);
     }
 
     // Asynchronic function to send the data using a POST request 
     // Returns a bool wrapped in a Task (or something like that)
     private async Task<bool> sendData(PersonaModel persona)
     {
+        // Test with a PersonaModel object
+        // PersonaModel persona = new PersonaModel("Miguel", "Alejandro", "h", "2024-11-13", 1);
+
         string jsonData = JsonSerializer.Serialize(persona);
         Debug.WriteLine(jsonData);
         var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
@@ -70,6 +88,7 @@ public partial class FormPage : ContentPage
         catch (System.Exception ex)
         {
             Console.WriteLine(ex);
+            Debug.WriteLine(ex);
         }
 
         return false;
